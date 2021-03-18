946b16cf5c438e3c2439a5822163738ec6685eaf
lyon0114 <lyon0114@algonquinlive.com>
2021-03-18 17:52:32 -0400

    added church foreign key

--- database/migrations/2021_03_05_003443_add_church_fields_to_user.php
+++ database/migrations/2021_03_05_003443_add_church_fields_to_user.php
@@ -17,8 +17,8 @@ class AddChurchFieldsToUser extends Migration
             //
             $table->string('user_type')->default(0);
             $table->boolean('active')->default(true);
-            $table->unsignedBigInteger('church_id')->default(0);
-            //$table->foreign('church')->references('id')->on('churches');
+            $table->unsignedBigInteger('church_id');
+            $table->foreign('church_id')->references('id')->on('churches');
 
         });
     }
